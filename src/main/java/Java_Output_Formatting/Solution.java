4bbcc7760c7293c4bde0c49c8d8cdf550c624937
sharif-jobayed <sharif.jobayed@gmail.com>
2024-09-25 20:29:57 +0600

    Complete Java_Output_Formatting

--- src/main/java/Java_Output_Formatting/Solution.java
+++ src/main/java/Java_Output_Formatting/Solution.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+
         System.out.println("================================");
         for(int i=0;i<3;i++)
         {
@@ -12,24 +13,9 @@ public class Solution {
             int x=sc.nextInt();
 
             //Complete this line
-            StringBuilder s2 = new StringBuilder();
-            for (int y = 0; y < 15; y++) {
-                if (y < s1.length() && s1.length() <= 10) {
-                    s2.append(s1.charAt(y));
-                } else {
-                    s2.append(" ");
-                }
-            }
-            String n = "";
-            if (x < 10) {
-                n += "00" + x;
-            } else if (x < 100) {
-                n += "0" + x;
-            } else {
-                n += "" + x;
-            }
-            System.out.println(s2 + n);
+            System.out.printf("%-15s%03d%n", s1, x);
         }
         System.out.println("================================");
+
     }
 }
