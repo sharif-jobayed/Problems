9c9791ca214481968b55a42637c2c9a152e80df1
sharif-jobayed <sharif.jobayed@gmail.com>
2024-10-02 22:01:30 +0600

    Solve Manual_Sorting

---
+++ src/main/java/Manual_Sorting/Solution.java
@@ -0,0 +1,33 @@
+package Manual_Sorting;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution {
+    static ArrayList<Integer> sortedNumbers = new ArrayList<>();
+
+    static void getSortedNumbers() {
+        Scanner receivedInteger = new Scanner(System.in);
+        Integer comparingUnit = 0;
+
+        // Receive numbers multiple times
+        for(int i = 0; i < 5; i++) {
+            Integer n = receivedInteger.nextInt();
+
+            // Find the correct position to insert the number
+            int insertPosition = 0;
+            while (insertPosition < sortedNumbers.size() && sortedNumbers.get(insertPosition) < n) {
+                insertPosition++;
+            }
+
+            // Insert the number at the correct position
+            sortedNumbers.add(insertPosition, n);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        getSortedNumbers();
+        System.out.println("The sorted numbers are: "+sortedNumbers);
+    }
+}
