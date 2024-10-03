ea23dea43af8758d5080592a25e297690f13edae
sharif-jobayed <sharif.jobayed@gmail.com>
2024-10-03 22:10:17 +0600

    Solve Multiple_Entry_Check

---
+++ src/main/java/Multiple_Entry_Check/Solution.java
@@ -0,0 +1,37 @@
+package Multiple_Entry_Check;
+
+import java.util.HashSet;
+
+public class Solution {
+
+    public static boolean hasDuplicates(Integer... args) {
+
+        HashSet<Integer> numbers = new HashSet<>();
+
+        for (int i : args) {
+            System.out.println("The numbers set is now: "+numbers);
+            System.out.println("The i is now: "+ i);
+
+            if (numbers.contains(i)) {
+                return true;
+            }
+            numbers.add(i);
+
+            System.out.println("-----------------------------------------------------------------------");
+        }
+
+        return false;
+
+    }
+
+
+
+    public static void main(String[] args) {
+        boolean hasDuplicates = hasDuplicates(5, 7, 8, 10, 18, 8, 11, 5, 8, 0);
+        if (hasDuplicates) {
+            System.out.println("Duplicates found.");
+        } else {
+            System.out.println("No duplicates found.");
+        }
+    }
+}
