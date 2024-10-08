575e069b2137e65500c47c1fc6259e2d191ec8a7
sharif-jobayed <sharif.jobayed@gmail.com>
2024-10-09 01:42:59 +0600

    Solved Sort Elements Both Way

--- src/main/java/NumbersList/LargestElement.java
+++ src/main/java/NumbersList/LargestElement.java
@@ -1,4 +1,4 @@
-package LargestElement;
+package NumbersList;
 
 import java.util.Arrays;
 import java.util.List;
@@ -6,7 +6,7 @@ import java.util.List;
 public class LargestElement {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(12, 15, 864, 68, 63584, 16, 4, 6354, 1);
-        Integer largetNumber = 0;
+        Integer largetNumber = numbers.getFirst();
 
         for(int i = 0; i < numbers.size(); i++) {
             if(largetNumber < numbers.get(i)) {
--- src/main/java/NumbersList/SmallestElement.java
+++ src/main/java/NumbersList/SmallestElement.java
@@ -1,4 +1,4 @@
-package LargestElement;
+package NumbersList;
 
 import java.util.Arrays;
 import java.util.List;
