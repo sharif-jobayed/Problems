878a9cf5f41a19bc90b5097efcd3cd345436d00a
sharif-jobayed <sharif.jobayed@gmail.com>
2024-10-09 00:58:23 +0600

    Solved Smallest Element

---
+++ src/main/java/LargestElement/SmallestElement.java
@@ -0,0 +1,17 @@
+package LargestElement;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SmallestElement {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(12, 15, 864, 68, 63584, 16, 4, 6354, 1);
+        Integer smallestNumber = numbers.get(0);
+
+        for(int i = 0; i < numbers.size(); i++) {
+            smallestNumber = numbers.get(i);
+        }
+
+        System.out.println("The smallest number is: "+smallestNumber);
+    }
+}
