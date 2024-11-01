3d725152510eb591b3a49dd0d23610f231cde559
sharif-jobayed <sharif.jobayed@gmail.com>
2024-11-02 02:37:38 +0600

    Update .gitignore

--- src/main/java/NumbersList/SmallestElement.java
+++ src/main/java/NumbersList/SmallestElement.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class SmallestElement {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(12, 15, 864, 68, 63584, 16, 4, 6354, 1);
-        Integer smallestNumber = numbers.getFirst();
+        Integer smallestNumber = numbers.get(0);
 
         for (Integer number : numbers) {
             smallestNumber = number;
--- src/main/java/SumOfNumbersInTheList/Solution.java
+++ src/main/java/SumOfNumbersInTheList/Solution.java
@@ -1,23 +1,33 @@
 package SumOfNumbersInTheList;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Solution {
-    private static void sum(List<Integer> intList) {
-        Integer total = 0;
+//    private static void sum(List<Integer> intList) {
+//        Integer total = 0;
+//
+//        for (int i = 0; i < intList.size(); i++) {
+//            Integer currentTotal = total += intList.get(i);
+//            System.out.println("Current total is: " + currentTotal);
+//        }
+//
+//        System.out.println("The sum of the numbers is: " + total);
+//    }
 
-        for (int i = 0; i < intList.size(); i++) {
-            Integer currentTotal = total += intList.get(i);
-            System.out.println("Current total is: " + currentTotal);
+    private static void sumInteger(Integer myInteger) {
+        int total = 0;
+        String intToStr = myInteger.toString();
+
+        for (int i = 0; i < intToStr.length(); i++) {
+            int strToInt = Character.getNumericValue(intToStr.charAt(i));
+            total += strToInt;
         }
 
-        System.out.println("The sum of the numbers is: " + total);
+        System.out.println("The total of the given integer is: " + total);
     }
 
     public static void main(String[] args) {
-        List<Integer> intList = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-        sum(intList);
+//        List<Integer> intList = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+//        sum(intList);
+        Integer myInteger = 1234;
+        sumInteger(myInteger);
     }
 }
