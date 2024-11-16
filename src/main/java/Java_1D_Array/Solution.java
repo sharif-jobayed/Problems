37efdc4ff499156f1e218408c9e3af8854453c5a
sharif-jobayed <sharif.jobayed@gmail.com>
2024-11-16 16:20:22 +0600

    Pending solution in Java_1D_Array

---
+++ src/main/java/Java_1D_Array/Solution.java
@@ -0,0 +1,14 @@
+package Java_1D_Array;
+
+import java.util.Scanner;
+
+public class Solution {
+    private Scanner scanner = new Scanner(System.in);
+    private int n = scanner.nextInt();
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
