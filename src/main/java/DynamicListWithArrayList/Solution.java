42e26e2121df9ad44c7072236d37a752784b58f3
sharif-jobayed <sharif.jobayed@gmail.com>
2024-11-27 00:19:33 +0600

    Done Dynamic List with ArrayList

---
+++ src/main/java/DynamicListWithArrayList/Solution.java
@@ -0,0 +1,34 @@
+package DynamicListWithArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    private List<String> names_01;
+
+    public Solution() {
+        this.names_01 = new ArrayList<>(List.of());
+    }
+
+    public Solution addNames() {
+        this.names_01.add("Imran");
+        this.names_01.add("Wasim");
+        this.names_01.add("Waqar");
+        this.names_01.add("Saeed");
+        this.names_01.add("Inzi");
+
+        return this;
+    }
+
+
+    /*-------------------------------------------------------------------------------------------------------*/
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        solution.addNames();
+        solution.names_01.forEach(name -> {
+            System.out.println("The name is: "+ name);
+        });
+    }
+}
