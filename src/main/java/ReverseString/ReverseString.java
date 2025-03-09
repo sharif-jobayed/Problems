4cd6106842bd338984e74e85d2931cfe6a8df318
sharif-jobayed <sharif.jobayed@gmail.com>
2025-03-10 02:46:12 +0600

    Refactor ReverseString.java

--- src/main/java/ReverseString/ReverseString.java
+++ src/main/java/ReverseString/ReverseString.java
@@ -3,7 +3,14 @@ package ReverseString;
 public class ReverseString {
     public static void main(String[] args) {
         String text = "Hello";
-        String reverseString = new StringBuilder(text).reverse().toString();
-        System.out.println("The reverse String is: " + reverseString);
+        StringBuilder reversed = new StringBuilder();
+//        String reverseString = new StringBuilder(text).reverse().toString();
+//        System.out.println("The reverse String is: " + reverseString);
+
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reversed.append(text.charAt(i));
+        }
+
+        System.out.println("The reverse String is: " + reversed);
     }
 }
