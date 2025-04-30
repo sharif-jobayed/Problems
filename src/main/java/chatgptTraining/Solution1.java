f27ce8aa8fba2bb30ce564a0e7fa834eecab01e6
sharif-jobayed <sharif.jobayed@gmail.com>
2025-04-30 10:36:13 +0600

    Refactor Solution1.java

--- src/main/java/chatgptTraining/Solution1.java
+++ src/main/java/chatgptTraining/Solution1.java
@@ -3,30 +3,32 @@ package chatgptTraining;
 import java.util.Arrays;
 
 public class Solution1 {
-    public int[] arr1 = {98, 461, 61, 5, 84, 614, 1};
+    public int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
     public int[] arr2 = new int[10];
 
-    public void reverse(int[] arr) {
-        for (int i = 0; i < arr.length / 2; i++) {
-            int temp = arr[i];
-            arr[i] = arr[arr.length - 1 - i];
-            arr[arr.length - 1 - i] = temp;
+    public int[] revert1(int[] arr) {
+        int[] tempArr = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            tempArr[i] = arr[arr.length - 1 - i];
+            tempArr[arr.length - 1 - i] = arr[i];
         }
+        return tempArr;
     }
+
 
     public static void main(String[] args) {
         Solution1 solution1 = new Solution1();
 
         System.out.println("Stock arr1 is: " + Arrays.toString(solution1.arr1));
-        solution1.reverse(solution1.arr1);
-        System.out.println("Reversed arr1 is: " + Arrays.toString(solution1.arr1));
+        int[] revertArr1 = solution1.revert1(solution1.arr1);
+        System.out.println("Reversed arr1 is: " + Arrays.toString(revertArr1));
 
         for (int i = 0; i < solution1.arr2.length; i++) {
             solution1.arr2[i] = i;
         }
-
-        System.out.println("arr2 with values is: " + Arrays.toString(solution1.arr2));
-        solution1.reverse(solution1.arr2);
-        System.out.println("Reversed arr2 is: " + Arrays.toString(solution1.arr2));
+        System.out.println("The array2 is: " + Arrays.toString(solution1.arr2));
+        int[] revertArr2 = solution1.revert1(solution1.arr2);
+        System.out.println("The reversed array2 is: " + Arrays.toString(revertArr2));
     }
 }
---
+++ src/main/java/chatgptTraining/Solution2.java
@@ -0,0 +1,19 @@
+package chatgptTraining;
+
+public class Solution2 {
+    public int[] arr1 = {98, 461, 61, 5, 84, 614, 1};
+
+    public int sum(int[] arr) {
+        int sum = 0;
+        for (int j : arr) {
+            sum += j;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Solution2 solution2 = new Solution2();
+        System.out.println("Sum of arr1 is: " + solution2.sum(solution2.arr1));
+    }
+
+}
